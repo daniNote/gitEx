78dc7cdd692de9ea831851c1db61e6a95f7b1b3d
daeun <sde3300@gmail.com>
2025-06-23 16:31:50 +0900

    깃 실습 다시 커밋

---
+++ src/Git2.java
@@ -0,0 +1,7 @@
+public class Git2 {
+    public static void main(String[] args) {
+        System.out.println("두번째 커밋을 위한 파일");
+        // 1. 현재 브랜치를 여기로 재설정
+        // 2. 소프트 : 기존 파일을 살리면서 커밋 취소(이전 헤더로 이동)
+    }
+}
