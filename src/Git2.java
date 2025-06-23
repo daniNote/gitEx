9dea50f87fa95bf18c75fc01ce7ea5da5f6fa4f0
daeun <sde3300@gmail.com>
2025-06-23 16:37:17 +0900

    깃 실습 3

--- src/Git2.java
+++ src/Git2.java
@@ -2,6 +2,8 @@ public class Git2 {
     public static void main(String[] args) {
         System.out.println("두번째 커밋을 위한 파일");
         // 1. 현재 브랜치를 여기로 재설정
-        // 2. 소프트 : 기존 파일을 살리면서 커밋 취소(이전 헤더로 이동)
+        // 2. Soft : 기존 파일을 살리면서 커밋 취소(이전 헤더로 이동)
+        // 3. Hard : 파일을 아예 삭제하면서 커밋 취소
+        // 커밋 실행 취소(이전 단계로 되돌림)
     }
 }
