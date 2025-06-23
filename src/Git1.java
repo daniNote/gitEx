3b337c82ef9b764a8700d2f341578726f984f9ae
daeun <sde3300@gmail.com>
2025-06-23 16:24:39 +0900

    깃 실습

---
+++ src/Git1.java
@@ -0,0 +1,18 @@
+public class Git1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        // 커맨드+백스페이스 한줄 지우기
+        // 버전 관리 - Git 저장소 생성 - git init
+
+        // 깃 디렉토리 지우기
+
+        // cd(이동해라) ~(내 홈 디렉터리)
+        // cd ~
+
+        // ls -la(숨겨진 파일 다보기)
+        // ls Force -- 윈도우
+
+        // rm -rf(밑에 있는거 다지우겠다) 조심히써야함
+        // rm -rf 디렉토리명
+    }
+}
